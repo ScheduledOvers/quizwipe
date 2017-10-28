71f170265906964dcec7240661f8a11686ee0215
James C <3661681+JamesMCo@users.noreply.github.com>
2017-10-28 22:44:45 +0100

    Added question count dropdown to start page

--- 10ft/qw.js
+++ 10ft/qw.js
@@ -1,7 +1,14 @@
 function new_session()
 {
     $.ajax({
-        url: "/backend/session/new?name=" + $("#session_name").val()
+        url: "/backend/session/new?name=" + $("#session_name").val(),
+        success: function(data)
+        {
+            if (data["status"] == 0)
+            {
+                window.location = "/joining.html?sessionname=" + $("#session_name").val() + "&noquestions=" + $("#noquestions").val();
+            }
+        }
     });
 };
 
@@ -11,3 +18,7 @@ function get_player_list()
         url: "/backend/session/playerlist" + $.now()
     });
 };
+
+$(document).ready(function() {
+    $('select').material_select();
+});
