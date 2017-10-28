5d97951f55ffdde73d2900039bcc94ac5fb52e1a
Alexander Dewfall <seraphofthemaelstrom@googlemail.com>
2017-10-28 19:08:25 +0100

    Added player list return function

--- backend/backend.py
+++ backend/backend.py
@@ -7,5 +7,12 @@ sessions = {}
 def sessioninit(name,noquestions):
     if name in sessions:
         return {"status": 1}
-    sessions[name] = {"questions_count": noquestions, "players":{}}
+    sessions[name] = {"questions_count": noquestions, "players":{"flip":0,"flop":2,"fly":89}}
     return {"status": 0}    
+
+@hug.get("/backend/session/playerlist", output=hug.output_format.json)
+def playerlist(name):
+    if name in sessions:
+        return sessions[name]["players"].keys()
+    return {"status":1}
+    
