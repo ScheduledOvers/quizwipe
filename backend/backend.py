188b89ad1ec82134073e00a039644d8941ac5d9f
Alexander Dewfall <seraphofthemaelstrom@googlemail.com>
2017-10-28 18:45:17 +0100

    sessioninit added

--- backend/backend.py
+++ backend/backend.py
@@ -2,4 +2,10 @@
 import json, hug
 with open("questions.json") as f:
     questions = json.load(f)
-print(questions)
+sessions = {}
+@hug.get("/backend/session/new", output=hug.output_format.json) 
+def sessioninit(name,noquestions):
+    if name in sessions:
+        return {"status": 1}
+    sessions[name] = {"questions_count": noquestions, "players":{}}
+    return {"status": 0}    
