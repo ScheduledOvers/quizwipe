567809b763e191042911ae3a99eebc292abcc185
James C <3661681+JamesMCo@users.noreply.github.com>
2017-10-29 02:40:25 +0000

    Start page now checks if there is a session name before submission

--- 10ft/qw.js
+++ 10ft/qw.js
@@ -1,15 +1,18 @@
 function new_session()
 {
-    $.ajax({
-        url: "/backend/session/new?sessionName=" + $("#session_name").val() + "&noquestions=" + $("#noquestions").val(),
-        success: function(data)
-        {
-            if (data["status"] == 0)
+    if ($("#session_name").val() !== "")
+    {
+        $.ajax({
+            url: "/backend/session/new?sessionName=" + $("#session_name").val() + "&noquestions=" + $("#noquestions").val(),
+            success: function(data)
             {
-                window.location = "/10ft/joining.html?sessionName=" + $("#session_name").val();
+                if (data["status"] == 0)
+                {
+                    window.location = "/10ft/joining.html?sessionName=" + $("#session_name").val();
+                }
             }
-        }
-    });
+        });
+    }
 };
 
 function get_player_list()
@@ -22,8 +25,7 @@ function get_player_list()
             if (data != playerlist)
             {
                 playerlist = data;
-                $("#playerdisplay").html("");
-                out = ""
+                out = "";
                 for (player in data)
                 {
                     out += "<div class=\"col s3\" style=\"text-align: center;\"><img src=\"https://api.adorable.io/avatars/285/" + playerlist[player] + ".png\" style=\"height: 4em; border-radius: 20%;\"><br><span class=\"title\">" + playerlist[player] + "<br><br></span></div>";
